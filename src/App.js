663d31550e580fc4f3c86c7bfd7bc6c39f8a7e78
cakiran <cakiran@gmail.com>
2018-11-03 18:31:59 -0400

    added redux components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,71 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import StudentList from './StudentList.jsx';
+import {connect} from 'react-redux';
+import { bindActionCreators } from 'redux';
+import {addStudent,deleteStudent,updateStudent} from './actions/studentActions'
 
 class App extends Component {
+  constructor(props)
+  {
+    super(props);
+    this.addNewStudent = this.addNewStudent.bind(this);
+    this.deleteStudent = this.deleteStudent.bind(this);
+    this.editStudentSubmit = this.editStudentSubmit.bind(this);
+  }
+  componentWillMount(){
+
+  }
+  addNewStudent()
+  {
+this.props.addStudent({id:Math.max(...this.props.studentList.map(function(o){return o.id})) + 1,name:'',grade:1,school:''});
+  }
+
+  deleteStudent(id)
+  {
+    let r = window.confirm("Do you want to delete this item");
+    if( r === true)
+    {
+    this.props.deleteStudent(id);
+   
+  }
+  }
+  editStudentSubmit(id,name,grade,school)
+  {
+this.props.updateStudent({id:id,name:name,grade:grade,school:school});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="container-fluid">
+      <div className="row mt-3"><div className="col-lg-12">
+      <div className="card">
+  <div className="card-header">
+    Student Registry
+  </div>
+  <div className="card-body">
+  <table className="table table-hover">
+          <thead className="thead-dark"><tr><th>Name</th><th>Grade</th><th>School</th><th>Edit/Save</th><th>Delete</th></tr></thead>
+          <StudentList deleteStudent={this.deleteStudent} studentList={this.props.studentList} editStudentSubmit={this.editStudentSubmit}/>
+        </table>
+        <button className="btn btn-dark pull-left" onClick={this.addNewStudent}>Add New</button>
+      </div></div></div></div></div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    studentList : state
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    addStudent:addStudent,
+    deleteStudent:deleteStudent,
+    updateStudent:updateStudent
+  },dispatch);
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+import studentReducer from './reducers/studentReducer';
+import {Provider} from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let initialState = [{id:1,name:'John Doe',grade:1,school:'React Redux School'},{id:2,name:'Jane Doe',grade:2,school:'React Redux School'}
+,{id:3,name:'Terry Adams',grade:3,school:'React Redux School'},{id:4,name:'Jenny Smith',grade:4,school:'React Redux School'}];
+
+if( localStorage.getItem("students") === null)
+localStorage.setItem('students',JSON.stringify(initialState));
+else 
+initialState = JSON.parse(localStorage.getItem('students'));
+
+const store = createStore(studentReducer,initialState);
+
+
+ReactDOM.render(
+<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
